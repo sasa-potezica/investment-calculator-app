93c8546237e4b416519fdf122eed0bdc1f042413
sasa-potezica <sasapotezica@yahoo.com>
2026-03-05 22:54:43 +0100

    Show results in table format

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,37 @@
 import React from "react";
-import calculateInvestmentResults from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
+import { renderToReadableStream } from "react-dom/server";
 
 const Results = ({ userInput }) => {
-  return (resultsData = calculateInvestmentResults(userInput));
+  const resultsData = calculateInvestmentResults(userInput);
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const totalInterestValue =
+            yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{}</td>
+              <td>{}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default Results;
